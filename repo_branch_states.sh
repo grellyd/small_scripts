02958529f63784bb518585946ae2949d650784d4
Graham Brown <graham.brown@lendesk.com>
2017-06-29 11:30:03 -0700

    Added repo branch states shell script.

---
+++ repo_branch_states.sh
@@ -0,0 +1,34 @@
+### Prints out the directories and Git branches directly below.
+### Calls into each directory one level below the calling directory.
+### Very handy when developing with lots of services.
+
+DARK_BLUE='\033[11;34m'
+PURPLE='\033[01;35m'
+NO_COLOUR='\033[0m'
+
+# parses the local git branch
+function parse_git_branch {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
+
+# pretty prints the folder name and the branch
+# $1 folder name
+# $2 branch name
+function print_branch_info () {
+  # set the default branch message
+  repo=${2:-'---- Not a Git repository'}
+  # pretty print folder name
+  echo -e "$DARK_BLUE$1$NO_COLOUR: $PURPLE$repo$NO_COLOUR"
+}
+
+function main {
+  # for each item one level below
+  for dir in *;
+  do 
+    # check is actually a directory
+    test -d $dir || continue
+    # run parse_git_branch in a subshell
+    ( cd $dir && print_branch_info $dir $(parse_git_branch) )
+  done
+}
+main
