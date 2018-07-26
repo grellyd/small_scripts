cef1ed0b77104d2302e273a445947c64ab4a2974
grellyd <graham.brown137@gmail.com>
2018-07-25 17:05:24 -0700

    Extracted out to separate scripts as the env var modification was inconsitently working due to sourcing

---
+++ workspace_external.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+xrandr --output HDMI1 --auto --left-of eDP1 --nograb
+xrandr --output eDP1 --scale 0.6x0.6
---
+++ workspace_local.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+xrandr --output HDMI1 --off
+xrandr --output eDP1 --scale 1.0x1.0
